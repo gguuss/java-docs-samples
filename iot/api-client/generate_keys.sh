c6d8528df344a4ffe0779d845baaf8feeb1a0f37
Gus Class <gguuss@gmail.com>
2018-02-17 22:34:12 -0800

    Adds explicit expiry on X509 RSA cert

--- iot/api-client/generate_keys.sh
+++ iot/api-client/generate_keys.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out \
-    rsa_cert.pem -subj "/CN=unused"
+openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -days 100 \
+    -nodes -out rsa_cert.pem -subj "/CN=unused"
 openssl ecparam -genkey -name prime256v1 -noout -out ec_private.pem
 openssl ec -in ec_private.pem -pubout -out ec_public.pem
 openssl pkcs8 -topk8 -inform PEM -outform DER -in rsa_private.pem \
